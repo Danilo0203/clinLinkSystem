451ed233e143e58e8df7d3820e5fedf0604376bf
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-10-02 23:45:35 -0600

    [UPDATE]: Añadir configuración experimental para CSRBailout en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
 module.exports = {
     env: {
-        NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+        NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST
     },
-}
+    experimental: {
+        missingSuspenseWithCSRBailout: false
+    }
+};
